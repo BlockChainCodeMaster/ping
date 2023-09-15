4e220e16506f824ed515755ef6f33d9508bd96c4
aemoe <aemoe@icloiud.com>
2023-09-15 16:51:46 +0800

    commit

---
+++ pages/header.tsx
@@ -0,0 +1,10 @@
+import { Web3Button } from "@web3modal/react";
+
+export default function Header() {
+  return (
+    <header className="flex justify-center">
+      header
+      <Web3Button />
+    </header>
+  );
+}
